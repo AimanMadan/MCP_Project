6e5f8af19c00c3dfa75854e60c6531ef3a14aefe
Aiman <64807286+AimanMadan@users.noreply.github.com>
2025-05-20 01:31:49 -0700

    server is ready with first function

--- server.py
+++ server.py
@@ -11,12 +11,13 @@ def test():
 
 @mcp.route("/tools/list_files", methods=["POST"])
 def list_files_tool():
+    
     data = request.get_json() # checks if the HTTP request has any JSON object
+    
     if not data or "path" not in data:
         return jsonify({"error": "Missing 'path' in request body"}), 400
     
     relative_path = data["path"]
-    
     result = tools.list_files_implementation(relative_path)
     
     
@@ -25,12 +26,16 @@ def list_files_tool():
         error_message = result["error"]
         if "Path not found" in error_message:
             return jsonify(result), 404  # Not Found
+        
         elif "Invalid path: '..' sequence not allowed." in error_message:
             return jsonify(result), 400  # Bad Request
+        
         elif "Invalid path: Attempt to access outside the base directory." in error_message:
             return jsonify(result), 403  # Forbidden
+        
         elif "Path is not a directory" in error_message:
-            return jsonify(result), 400  # Bad Request (resource is not the correct type for the operation)
+            return jsonify(result), 400  # Bad Request 
+        
         elif "An unexpected error occurred" in error_message:
             # log unexpected server errors
             mcp.logger.error(f"Unexpected server error for path '{relative_path}': {error_message}")
